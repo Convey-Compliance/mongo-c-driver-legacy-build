293694a20d889765f74d26129f89f7424c742d83
Jose Sebastian Battig <jsbattig@convey.com>
2014-03-21 15:20:55 -0500

    Added code to sign release and debug versions

--- build/sign.cmd
+++ build/sign.cmd
@@ -1,5 +1,7 @@
 set workspace=%~dp0
 pushd X:\__Dev\__Source\build-toolbox.development\CodeSign
-call SignIt_signtool.bat %workspace%..\bin\Win32\%1\MongoC.dll
-call SignIt_signtool.bat %workspace%..\bin\x64\%1\MongoC.dll
+call SignIt_signtool.bat %workspace%..\bin\Win32\Debug\mongoc_d32_v1-0-1.dll
+call SignIt_signtool.bat %workspace%..\bin\x64\Debug\mongoc_d64_v1-0-1.dll
+call SignIt_signtool.bat %workspace%..\bin\Win32\Release\mongoc_r32_v1-0-1.dll
+call SignIt_signtool.bat %workspace%..\bin\x64\Release\mongoc_r64_v1-0-1.dll
 popd
