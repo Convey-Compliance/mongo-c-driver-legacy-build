a71e1e0c171c572c5095c24f260fbf959d1ae6ba
Kyle Moon <kmoon@convey.com>
2014-10-28 16:49:09 -0500

    Remove copyall in order to copy to network share

--- build/publish.cmd
+++ build/publish.cmd
@@ -1,6 +1,8 @@
-robocopy ..\bin\Win32\Debug\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /COPYALL /TEE
-robocopy ..\bin\Win32\Release\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /COPYALL /TEE
-robocopy ..\bin\x64\Debug\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /COPYALL /TEE
-robocopy ..\bin\x64\Release\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /COPYALL /TEE
+@ECHO OFF
+
+robocopy ..\bin\Win32\Debug\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /TEE
+robocopy ..\bin\Win32\Release\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /TEE
+robocopy ..\bin\x64\Debug\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /TEE
+robocopy ..\bin\x64\Release\ %1\user\mongoc-dll\ mongoc*.dll /ZB /X /TEE
 
 EXIT /B 0
